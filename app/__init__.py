2d2889719782eaa4c1bb0d864fd2535d8c7693a7
Npanda04 <deepanshusisodiya21@gmail.com>
2024-03-10 15:22:08 +0530

    modify dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,23 @@
+import os
+
 from flask import Flask
 from app.models import db, ma
 from app.routes.userRoutes import user_routes_bp
 from flask_migrate import Migrate
 from flask_cors import CORS
+from dotenv import load_dotenv
+
+
+
 
 def create_app():
     app = Flask(__name__)
+    load_dotenv()
     CORS(app)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://learn.deepanshu:nCJW4YR9DUPV@ep-orange-pine-a5e2k0bs.us-east-2.aws.neon.tech/flask?sslmode=require"
+    backend_url = os.getenv("BACKEND_URL")
 
+    app.config["SQLALCHEMY_DATABASE_URI"] = backend_url
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
